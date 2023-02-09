17e32042e663095cb6789fd055fdfd9815447e8d
ar1stey <90979777+ar1stey@users.noreply.github.com>
2023-02-09 17:58:28 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,36 @@
+/*alert("Hello World");
+alert("Привіт Світ");
+alert(3+
+	25+
+	11)//тут у мене сума чисел
+let ourPlanetName;
+ourPlanetName='Земля!'
+alert(ourPlanetName)
+
+let userName;
+userName="Василь"
+alert(userName)
+
+let user, name;
+name="В'ячеслав";
+user=name;
+alert(user)*/
+
+
+
+//alert(9 % 2)
+
+/*let n = 2;
+n += 5;
+n *= 2;
+alert(n)*/ ;
+
+
+let str1="Мовчання - ", str2="найкращий спосіб ", str3="відповіді на", str4="безглузді ",str5="питання"
+document.write("<p>"+str1+str2+str3+str4+str5+"</p>")
+
+let name=prompt('What is your namme')
+alert("Hello dear "+name+"!")
+
+
+// це було 10.01.2023
